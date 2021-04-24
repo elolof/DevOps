96708fa04e983f2d70a77c09e685b7e9223ec821
elolof <47170695+elolof@users.noreply.github.com>
2021-04-24 21:22:18 +0300

    Update testi.py

--- Nisan19/testi.py
+++ Nisan19/testi.py
@@ -13,17 +13,29 @@ mysql = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.headers.get("bootcamp") == "devops":
+        return render_template('devops.html')
     if request.method == "POST":
         details = request.form
         kilo = details['weight']
         boy = details['height']
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO sizes(kilo, boy) VALUES (%i, %i)", (kilo, boy))
+        insert_query = """INSERT INTO sizes(kilo, boy) VALUES (%s, %s)"""
+        cur.execute(insert_query,(kilo, boy))
         mysql.connection.commit()
         cur.close()
         return 'success'
     return render_template('index.html')
 
+@app.route('/list', methods=['GET'])
+def list():
+    cur = mysql.connection.cursor()
+    get_query = """SELECT boy, kilo FROM sizes"""
+    cur.execute(get_query)
+    result=cur.fetchall()
+    mysql.connection.commit()
+    cur.close()
+    return str(result)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
